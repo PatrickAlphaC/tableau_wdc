c8e80cf9318db03e435421e5709add470fcdc78c
PatrickAlphaVantage <patrick.collins@alphavantage.co>
2019-10-01 19:28:53 -0400

    Working edition of multiple tickers

--- tableau_wdc.js
+++ tableau_wdc.js
@@ -4,69 +4,93 @@
 
     // Define the schema
     myConnector.getSchema = function (schemaCallback) {
-        var timeseries_columns = [{
-            id: "timestamp",
-            dataType: tableau.dataTypeEnum.datetime
-        }, {
-            id: "open",
-            alias: "open",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "high",
-            alias: "high",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "low",
-            alias: "low",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "close",
-            alias: "close",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "volume",
-            alias: "volume",
-            dataType: tableau.dataTypeEnum.float
-        }];
-
-        var tableSchema = {
-            // TODO, give the timeseries the tickername
-            id: "timeseries",
-            alias: "Timeseries for TICKER_HERE",
-            columns: timeseries_columns
-        };
-
-        schemaCallback([tableSchema]);
+        // Get the list of symbols
+        var symbol_list = JSON.parse(tableau.connectionData).symbol_list.split(",");
+        var list_of_schemas = new Array();
+        for (symbol in symbol_list) {
+            var timeseries_columns = [{
+                id: "symbol",
+                alias: "symbol",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "timestamp",
+                dataType: tableau.dataTypeEnum.datetime
+            }, {
+                id: "open",
+                alias: "open",
+                dataType: tableau.dataTypeEnum.float
+            }, {
+                id: "high",
+                alias: "high",
+                dataType: tableau.dataTypeEnum.float
+            }, {
+                id: "low",
+                alias: "low",
+                dataType: tableau.dataTypeEnum.float
+            }, {
+                id: "close",
+                alias: "close",
+                dataType: tableau.dataTypeEnum.float
+            }, {
+                id: "volume",
+                alias: "volume",
+                dataType: tableau.dataTypeEnum.float
+            }];
+            list_of_schemas.push({
+                // TODO, give the timeseries the symbolname
+                id: symbol_list[symbol],
+                alias: "Timeseries for " + symbol_list[symbol],
+                columns: timeseries_columns,
+                // symbol: symbol_list[symbol]
+            })
+        }
+        schemaCallback(list_of_schemas);
     };
 
     // Download the data
     myConnector.getData = function (table, doneCallback) {
-        apicall = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+        var baseurl = "https://www.alphavantage.co/query?function=";
+        var function_key = "TIME_SERIES_INTRADAY";
+        var query_data = JSON.parse(tableau.connectionData);
+        var symbol = table.tableInfo.id;
+        var apicall = baseurl + function_key + "&symbol=" + symbol + "&interval=1min&apikey=" + query_data.api_key;
+        //var apicall = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=NL2O0F4U4GBPOQMN";
         $.getJSON(apicall, function (resp) {
-            tableData = [];
-            // Iterate over the JSON object
+            // var tableData = [];
+            // for (data_metadata in resp) {
+            //     // "Time Series (5min)"
+            //     for (timeseries in resp[data_metadata]) {
+            //         tableData.push({
+            //             "symbol": symbol,
+            //             "timestamp": timeseries,
+            //             "open": resp[data_metadata][timeseries]["1. open"],
+            //             "high": resp[data_metadata][timeseries]["2. high"],
+            //             "low": resp[data_metadata][timeseries]["3. low"],
+            //             "close": resp[data_metadata][timeseries]["4. close"],
+            //             "volume": resp[data_metadata][timeseries]["5. volume"]
+            //         });
+            //     }
+            // }
+
+            var tableData = [];
             var index = 0;
             for (data_metadata in resp) {
                 if (index == 1) {
                     for (timeseries in resp[data_metadata]) {
-                        for (data in resp[data_metadata][timeseries]) {
-                            tableData.push({
-                                "timestamp": timeseries,
-                                "open": resp[data_metadata][timeseries]["1. open"],
-                                "high": resp[data_metadata][timeseries]["2. high"],
-                                "low": resp[data_metadata][timeseries]["3. low"],
-                                "close": resp[data_metadata][timeseries]["4. close"],
-                                "volume": resp[data_metadata][timeseries]["5. volume"]
-                            });
-                        }
-
-                        document.getElementById("output").innerHTML = timeseries;
+                        tableData.push({
+                            "symbol": symbol,
+                            "timestamp": timeseries,
+                            "open": resp[data_metadata][timeseries]["1. open"],
+                            "high": resp[data_metadata][timeseries]["2. high"],
+                            "low": resp[data_metadata][timeseries]["3. low"],
+                            "close": resp[data_metadata][timeseries]["4. close"],
+                            "volume": resp[data_metadata][timeseries]["5. volume"]
+                        });
                     }
+                    table.appendRows(tableData);
                 }
                 index = index + 1;
-
             }
-            table.appendRows(tableData);
             doneCallback();
         });
     };
@@ -75,13 +99,19 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function () {
         $("#submitButton").click(function () {
-            var dateObj = {
-                startDate: $('#ticker-list').val().trim(),
-            };
-            tableau.connectionData = JSON.stringify(dateObj); // Use this variable to pass data to your getSchema and getData functions
-            //TODO Ticker name here
-            tableau.connectionName = "Ticker feed"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
+            if (document.getElementById("symbol-list").value == "" || document.getElementById("api-key").value == "") {
+                document.getElementById("output").innerHTML = "Please enter symbols and API Key";
+            } else {
+                var query_data = {
+                    symbol_list: $('#symbol-list').val().trim().replace(/\s/g, ''),
+                    api_key: $('#api-key').val().trim().replace(/\s/g, ''),
+                };
+                // can't send it in list form wtf
+                tableau.connectionData = JSON.stringify(query_data); // Use this variable to pass data to your getSchema and getData functions
+                // TODO symbol name here
+                tableau.connectionName = query_data.symbol_list + " Time Series"; // This will be the data source name in Tableau
+                tableau.submit(); // This sends the connector object to Tableau
+            }
         });
     });
 })();
